7b84000dc2b47e4c742eb2c2b95c1608e72fa3ad
cnaps <scant10@gmail.com>
2023-03-29 23:55:41 +0900

    update test code

--- src/main/java/com/inflearn/rentalcard/RentalCardApplication.java
+++ src/main/java/com/inflearn/rentalcard/RentalCardApplication.java
@@ -20,69 +20,73 @@ public class RentalCardApplication {
     public static void domainTest() throws Exception {
 
 
-        System.out.println("1,2,3,4,5권 대여");
-        System.out.println("5번 반납");
-        System.out.println("1,2,4번 연체");
-        System.out.println("4번 반납");
-        System.out.println("1,2,3번 연체");
-
         System.out.println("------------도메인 모델 테스트 진행------------");
+        System.out.println("도서카드 생성");
         RentalCard sampleCard = RentalCard.sample();
-        System.out.println(sampleCard.getMember().getName());
+        System.out.println("도서카드명 " + sampleCard.getMember().getName());
+        System.out.println("-----------------------------------------");
+    
+        var sample1 = new Item(1,"노인과바다");
+        var smaple2 = new Item(2, "홍길동전");
+        var sample3 = new Item(3,"누구를 위하여 종을 울리나?");
 
-        var sample3 = new Item(2,"노인과바다");
-        var smaple4 = new Item(3, "홍길동전");
-        var sample5 = new Item(4,"누구를 위하여 종을 울리나?");
-
-        System.out.println("-------샘플3 대여하기---------------------");
+        System.out.println(sample1.getTitle()+" ------- Rent ------");
+        sampleCard.rentItem(sample1);
+        System.out.println(smaple2.getTitle()+"------- Rent ------");
+        sampleCard.rentItem(smaple2);
+        System.out.println(sample3.getTitle()+"------- Rent ------");
         sampleCard.rentItem(sample3);
-        System.out.println("-------샘플4 대여하기---------------------");
-        sampleCard.rentItem(smaple4);
-        System.out.println("-------샘플1 대여하기---------------------");
+        System.out.println(Item.sample().getTitle() + " ------- Rent ------");
         sampleCard.rentItem(Item.sample());
-
-        System.out.println(sampleCard.getMember().getName());
+        System.out.println("-----------------------------------------");
+    
+        System.out.println(sampleCard.getMember().getName() + "-------대여목록-------");
         System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
+        System.out.println(sampleCard.getMember().getName() + "-------반납목록-------");
         System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
-
-        System.out.println("-------샘플1 반납하기---------------------");
+        System.out.println("-----------------------------------------");
+    
+        System.out.println(Item.sample().getTitle()+" ------- Return ------");
         sampleCard.returnItem(Item.sample(), LocalDate.now());
-        System.out.println(sampleCard.getMember().getName());
+        
+        System.out.println(sampleCard.getMember().getName() + "-------대여목록-------");
         System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
+        System.out.println(sampleCard.getMember().getName() + "-------반납목록-------");
         System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
-
-        System.out.println("-------샘플2 대여하기---------------------");
+        System.out.println("-----------------------------------------");
+    
+        System.out.println(Item.sample2().getTitle() + " ------- Rent ------");
         sampleCard.rentItem(Item.sample2());
-        System.out.println(sampleCard.getMember().getName());
-        System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
-        System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
 
-        System.out.println("-------샘플2 연체하기---------------------");
+        System.out.println(sampleCard.getMember().getName() + "-------대여목록-------");
+        System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
+        System.out.println(sampleCard.getMember().getName() + "-------반납목록-------");
+        System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
+        System.out.println("-----------------------------------------");
+    
+        System.out.println(Item.sample2().getTitle() + " ------- Overdue ------");
         sampleCard.overdueItem(Item.sample2());
-        System.out.println("-------샘플3 연체하기---------------------");
+        System.out.println(sample3.getTitle() + " ------- Overdue ------");
         sampleCard.overdueItem(sample3);
 
-
-        System.out.println(sampleCard.getMember().getName());
+        System.out.println(sampleCard.getMember().getName() + "-------대여목록-------");
         System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
+        System.out.println(sampleCard.getMember().getName() + "-------반납목록-------");
         System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
-
-        System.out.println("-------현재도서카드 상태---------------------");
-        System.out.println(sampleCard.getMember().getName());
-        System.out.println(sampleCard.getRentItemList().stream().map(m->m.isOverdued()).collect(Collectors.toList()));
-        System.out.println(sampleCard.getTotalLateFee().getPoint());
-        System.out.println(sampleCard.getRentStatus().toString());
-
-
+       
+        System.out.println("-----------------------------------------");
+    
+        System.out.println(sampleCard.getMember().getName()+ "-------현재도서카드 상태--------------");
+        System.out.println("대여도서 상태 : "+sampleCard.getRentItemList().stream().map(m->m.isOverdued()).collect(Collectors.toList()));
+        System.out.println("총연체료: "+sampleCard.getTotalLateFee().getPoint());
+        System.out.println("대여가능여부: "+sampleCard.getRentStatus().toString());
 
         sampleCard.calculateLateFee();
 
-        System.out.println(sampleCard.getMember().getName());
-        System.out.println(sampleCard.getTotalLateFee().getPoint());
+        System.out.println("총연체료: "+sampleCard.getTotalLateFee().getPoint());
         System.out.println(sampleCard.getRentItemList().stream().map(m->m.getItem().getTitle()).collect(Collectors.toList()));
         System.out.println(sampleCard.getReturnItemList().stream().map(m->m.getItem().getItem().getTitle()).collect(Collectors.toList()));
 
-        sampleCard.rentItem(sample5);
 
 
     }
