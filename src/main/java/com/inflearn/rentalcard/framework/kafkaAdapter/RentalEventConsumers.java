ac592ed767a4341aa6ca31134b716d4136b2ccb7
scant10@gmail.com <scant10@gmail.com>
2023-07-10 23:03:19 +0900

    ede

---
+++ src/main/java/com/inflearn/rentalcard/framework/kafkaAdapter/RentalEventConsumers.java
@@ -0,0 +1,25 @@
+package com.inflearn.rentalcard.framework.kafkaAdapter;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+
+@Service
+@RequiredArgsConstructor
+public class RentalEventConsumers {
+
+    private final Logger log = LoggerFactory.getLogger(RentalEventConsumers.class);
+
+    public static final String TOPIC = "topic_kafka";
+
+    @KafkaListener(topics = "exam", groupId = "foo")
+    public void consume(String message) throws IOException {
+        System.out.printf("Consumed message : %s%n", message);
+    }
+}
---
+++ src/main/java/com/inflearn/rentalcard/framework/kafkaAdapter/RentalProducer.java
@@ -0,0 +1,30 @@
+package com.inflearn.rentalcard.framework.kafkaAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RentalProducer {
+
+    private final Logger log = LoggerFactory.getLogger(RentalProducer.class);
+
+    private static final String TOPIC = "exam";
+    private static final String TOPIC_CATALOG = "topic_catalog";
+    private static final String TOPIC_POINT = "topic_point";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    private KafkaProducer<String, String> producer;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public void sendMessage(String message) {
+        System.out.printf("Produce message : %s%n", message);
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
